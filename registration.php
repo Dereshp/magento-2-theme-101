80c00714d07f077428432b3c69c0a6b504ffad81
dereshp <dereshpillay@icloud.com>
2022-10-21 23:18:21 +0200

    stage changes

--- registration.php
+++ registration.php
@@ -4,6 +4,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::THEME,
-    'frontend/MageMastery/custom',
+    'frontend/Captive/pronko',
     __DIR__
 );
